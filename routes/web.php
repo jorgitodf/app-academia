4874ceda97e26853d64c9f142c7f798989b7d2c3
Jorgito Paiva <jspaiva.1977@gmail.com>
2023-09-18 18:15:27 -0300

    Criação Layout View

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Site\SiteController;
 use App\Http\Controllers\TypeUserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
 
+Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/tipo-usuario', [TypeUserController::class, 'index'])->name('type-user');
